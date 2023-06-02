651361ead58cd1e230c07725fe04284eaf54fc5d
Max Kryzhanovskiy <kryzhanovskiymax@mail.ru>
2023-06-02 10:40:04 +0300

    c

---
+++ task2.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+
+from matplotlib import pyplot as plt
+
+# визуализация состоятельности оценки
+# получение точек для визуализации
+theta = 0.5
+rands = np.array([])
+x = np.arange(1, 10001)
+y = np.array([])
+for i in range(0, 10000):
+    num = np.random.geometric(theta)
+    rands = np.append(rands, num)
+    param = 1 / np.mean(rands)
+    y = np.append(y, param)
+
+
+plt.plot(x, y)
+plt.axline((0, theta), (1, theta), color='r')
+plt.title('theta consistency')
+plt.ylabel('theta')
+plt.xlabel('dataset size')
+plt.show()
---
+++ test.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+
+vals = [
+    np.random.normal(size=50).tolist(),
+    np.random.normal(size=500).tolist(),
+    np.random.normal(size=10000).tolist()
+]
+
+# for i in range(0, 3):
+#     print(vals[i])
+
+deviation = lambda x: (x - m) ** 2
+
+print('For Expected Value without Deviation')
+for i in range(0, len(vals)):
+    s = np.array(vals[i])
+    n = np.size(s)
+    m = np.mean(s)
+    devs = deviation(s)
+    d = np.sqrt(1/(np.size(s)-1) * np.sum(devs))
+    delta = 2 * d / np.sqrt(np.size(s))
+    upper_bound = m + delta
+    lower_bound = m - delta
+    print(f'{n}: [{lower_bound}, {upper_bound}]')
+
+print('For Expected Value with Deviation')
+for i in range(0, len(vals)):
+    s = np.array(vals[i])
+    n = np.size(s)
+    m = np.mean(s)
+    delta = 2 * 1 / np.sqrt(np.size(s))
+    upper_bound = m + delta
+    lower_bound = m - delta
+    print(f'{n}: [{lower_bound}, {upper_bound}]')
+
+print('For Deviation without Expected Value')
+for i in range(0, len(vals)):
+    s = np.array(vals[i])
+    n = np.size(s)
+    m = np.mean(s)
+    devs = deviation(s)
+    d = (1/n) * np.sum(devs)
+    upper_bound = n * d / stats.chi2.ppf(1 - 0.025, n)
+    lower_bound = n * d / stats.chi2.ppf(1 - (1 - 0.025), n)
+    print(f'{n}: [{lower_bound}, {upper_bound}]')
+
+print('For Deviation with Expected Value')
+for i in range(0, len(vals)):
+    s = np.array(vals[i])
+    n = np.size(s)
+    m = 0
+    devs = deviation(s)
+    d = (1/n) * np.sum(devs)
+    lower_bound = n * d / stats.chi2.ppf(1 - 0.025, n)
+    upper_bound = n * d / stats.chi2.ppf(1 - (1 - 0.025), n)
+    print(f'{n}: [{lower_bound}, {upper_bound}]')
